e078c59003042503f1c3149f72d0ffd2c1ebd631
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2024-08-08 15:16:25 +0300

    main

---
+++ script.js
@@ -0,0 +1,214 @@
+const Examples = [
+  {
+    title: "Örnek 1",
+    description:
+      "Trendyol Sayfasındaki Navbar Menünün Sag Kısmında bulunan Giriş Yap Kısmında Hover Oldugunda Mouse Üzerine gelindiginde User İconunun Renginin Turuncu Olması Artı Olarak Alt kısmında Drop Menü açılması Ve içinde Giriş Yapma Ve üye olma Buttonlarının Bulunması ",
+    details:
+      "Yani Burada Javascript İle Bi hover işlemi Meydana geldiginde Style Degişmesi User İconunun Renginin Turuncu Olması Ve Alt Kısımda Normalde görünmezken Drop Menünün Açılması",
+    videoUrl: "/Örneklerin Videoları/Örnek-1-Video.mp4",
+  },
+  {
+    title: "Örnek 2",
+    description:
+      "Trendyol Sayfasındaki Navbar Menünün Sag Kısmında bulunan Giriş Yap Kısmında Hover Oldugunda Mouse Üzerine gelindiginde User İconunun Renginin Turuncu Olması Artı Olarak Alt kısmında Drop Menü açılması Ve içinde Giriş Yapma Ve üye olma Buttonlarının Bulunması ",
+    details:
+      "Yani Buradaki İşlem Sayfanın ScrollBarının Belli Degerden Çıktıktan sonra butonun Display:None İken Display:Block Olması Ve Posisiton Özelliginin Sticky Olması Ve Ona Tıklama İşlemi Oldugunda Sayfanın ScrollBarın 0 degeri Verilerek En üste Kaydırılması",
+    videoUrl: "/Örneklerin Videoları/Örnek-2-Video.mp4",
+  },
+  {
+    title: "Örnek 3",
+    description:
+      "Bu Örnekde W3School'Un Anasayfasında Navbarın Ortasında Bulunan Dark Ve light mode buttonları bulunuyor Ve Ona tıklandıgında İçinde Bulunan Circle'nın Normalde Sagda İken Sola Geçmesi Ve sayfada Navbar Ve Sayfanın Farklı Yerlerindeki Example Örneklerini Arkaplan rengini Koyulaştırması ",
+    details:
+      "Yani Burda Bazı Kısımların Arkaplan renginin koyulaşması Ve Mode Buttonlarının Normalde Sagdayken Tıklandıgında Sola geçmesi",
+    videoUrl: "/Örneklerin Videoları/Örnek-3-Video.mp4",
+  },
+  {
+    title: "Örnek 4",
+    description:
+      "  Google Ait Olan Gmailin oturum açma formunda Kullanıcı formu göndermeden  önce javascript ile form verilerini doğrular mesela e-posta adresinin geçerli olup olmadığını kontrol eder Hatalar varsa kullanıcıyı uyarır ",
+    details:
+      "yani burda form gönderildikten sonra sunucu eger eposta ve şifre dogru ise dogrular ve gmail hesabına yönlendirir yanlışsa sunucudan gelen hata mesajlarını işleyerek kullanıcıya hata mesajlarını gösterir",
+    videoUrl: "/Örneklerin Videoları/Örnek-4-Video.mp4",
+  },
+  {
+    title: "Örnek 5",
+    description: "Burdaki Örnegimiz İse Google Arama Çubugundan",
+    details:
+      "Arama kutusuna yazdıkça, JavaScript anında arama önerileri sunar AJAX çağrıları yaparak kullanıcı yazarken önerileri günceller. AJAX'In Anlamı: ufak miktarda veri değişimi sayesinde sayfayı daha hızlı güncelleyebilen web sayfaları yapmaktır",
+    videoUrl: "/Örneklerin Videoları/Örnek-5-Video.mp4",
+  },
+  {
+    title: "Örnek 6",
+    description: "Burdaki Örnegimiz eBay Satış sitesinden",
+    details:
+      "ürünlerin filtrelenmesi ve sıralanması için JavaScript kullanılır Kullanıcı filtre seçeneklerini değiştirdiğinde, JavaScript anında sonuçları günceller",
+    videoUrl: "/Örneklerin Videoları/Örnek-6-Video.mp4",
+  },
+  {
+    title: "Örnek 7",
+    description: "Google Calender Takvim Etkileşimi",
+    details:
+      "Etkinlikleri eklemek, düzenlemek ve görüntülemek için JavaScript kullanılır. Takvim etkileşimleri dinamik olarak güncellenir.",
+    videoUrl: "",
+  },
+  {
+    title: "Örnek 8",
+    description: "Intercom",
+    details:
+      "Kullanıcılara yardımcı olmak için konuşma balonları ve yardım mesajları JavaScript ile kontrol edilebilir Kullanıcı sayfayı gezdikçe yardımcı bilgiler sunar",
+    videoUrl: "",
+  },
+  {
+    title: "Örnek 9",
+    description: "AirTable",
+    details:
+      "Tablo verileri dinamik olarak düzenlenebilir. JavaScript, kullanıcıların tablo hücrelerini düzenlemesine ve verileri güncellemesine izin verir.",
+    videoUrl: "",
+  },
+  {
+    title: "Örnek 10",
+    description: "Google Maps",
+    details:
+      "Burda Yön tuşları ile kaydırma yapılabilme Veya Mouse İle sürükleme yapılabiliyor yönler ve etkileşimler JavaScript ile sağlanır. Kullanıcılar harita üzerinde gezinebilir ve yer bilgilerini görüntüleyebilir.",
+    videoUrl: "",
+  },
+  {
+    title: "Örnek 11",
+    description: "Buffer",
+    details:
+      "Sosyal medya hesaplarında kullanıcının içerik paylaşımı ve planlama işlemleri JavaScript ile yapılır. Kullanıcılar gönderileri belirli tarihlerde paylaşabilir",
+    videoUrl: "",
+  },
+  {
+    title: "Örnek 12",
+    description: "Linkedın",
+    details:
+      "Kullanıcı profil Veya hesap bilgilerinin ayarları javascript kullanılarak güncellenir profil-fotografı,biyografi,isim soyisim gibi veriler anlık olarak işlenir",
+    videoUrl: "",
+  },
+  {
+    title: "Örnek 13",
+    description: "Spotify",
+    details:
+      "Müzigin Çalması,duraklatılması, ses kontrolü şarkı playlistleri gibi işlemleri javascript ile ayarlanabiliyor ve dinamik bir müzik deneyimi sunabilir",
+    videoUrl: "",
+  },
+  {
+    title: "Örnek 14",
+    description: "Weather.com",
+    details:
+      "Ülkelerin Veya Şehirlerin Hava durumlarının bilgi veya tahminleri javascriptte güncellenir API'lerden alınan verilerle hava durumu veya grafikleri oluşturur",
+    videoUrl: "",
+  },
+  {
+    title: "Örnek 15",
+    description: "LottieFiles",
+    details:
+      " Animasyon dosyalarını paylaşan ve düzenleyen bir sitedir animasyonları dinamik olarak yüklemek ve etkileşimli hale getirmek için kullanılabilir",
+    videoUrl: "",
+  },
+  {
+    title: "Örnek 16",
+    description: "Patreon",
+    details:
+      "Yaratıcılar için destek platformudur JavaScript ile kullanıcı etkileşimlerini ödeme işlemlerini ve içerik erişimini yönetebilir",
+    videoUrl: "",
+  },
+  {
+    title: "Örnek 17",
+    description: "Unsplash",
+    details:
+      "Ücretsiz yüksek çözünürlüklü fotoğraflar Sunan Bi sitedir JavaScript ile fotoğraf arama   galeri oluşturma ve kullanıcı etkileşimleri için kullanılabilir",
+    videoUrl: "",
+  },
+  {
+    title: "Örnek 18",
+    description: "Code Pen",
+    details:
+      "Web geliştiricilerinin HTML CSS ve JavaScript kodlarını gerçek zamanlı olarak yazıp test edebildiği bir sitedir ve JavaScript  dinamik kod örneklerinin oluşturulması ve etkileşimli araçların sağlamasında kullanılır",
+    videoUrl: "",
+  },
+  {
+    title: "Örnek 19",
+    description: "GeoCaching",
+    details:
+      "Gerçek dünyada gizlenmiş hazineleri bulmak için kullanılan Sitedir JavaScript harita üzerinde koordinatları göstermek ve etkileşimli özellikleri yönetmek için kullanılabilir",
+    videoUrl: "",
+  },
+  {
+    title: "Örnek 20",
+    description: "Skribbl.io",
+    details:
+      "Çevrimiçi destegiyle çizim ve tahmin oyunu olan oyuna benzer sitedir çizim işlemleri ve oyun etkileşimli özellikleri js ile yapabiliriz",
+    videoUrl: "",
+  },
+  {
+    title: "Örnek 21",
+    description: "InVısion",
+    details:
+      "Prototip oluşturma ve tasarım işbirliği saglayan sitedir JavaScript etkileşimli prototipler, etkileşimli tasarım öğeleri ve kullanıcı geri bildirimleri için kullanılmaktadır.",
+    videoUrl: "",
+  },
+  {
+    title: "Örnek 22",
+    description: "Twitch",
+    details:
+      "Canlı Yayın platformu olan twitch ile Canlı sohbet,yayın kontrolleri mesajlaşma özellikleri ile javascriptte yapılabilir",
+    videoUrl: "",
+  },
+  {
+    title: "Örnek 23",
+    description: "Amazon",
+    details:
+      "İçerik Güncellemeleri Ve Medya etkileşimleri Amazon, ürünlerin sepete eklenmesi, stok durumlarının güncellenmesi ve dinamik arama sonuçları için JavaScript kullanır.",
+    videoUrl: "",
+  },
+  {
+    title: "Örnek 24",
+    description: "FaceBook",
+    details:
+      "Facebook, kullanıcıların sosyal medya hesaplarıyla giriş yapabilmesi ve Dogrulama işlemleri kullanıcı profillerini yönetebilmesi için JavaScript kullanır.",
+    videoUrl: "",
+  },
+  {
+    title: "Örnek 25",
+    description: "Khan Academy",
+    details:
+      "etkileşimli eğitim materyalleri ve testler için JavaScript kullanır, böylece öğrenciler öğrenme süreçlerini dinamik olarak yönetebilirler.",
+    videoUrl: "",
+  },
+];
+
+const exampleCard = document.getElementById("examplesCard");
+
+let exampleRender = true;
+
+function ExampleShow() {
+  if (exampleRender) {
+    Examples.forEach((example) => {
+      exampleCard.innerHTML += ` <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+            <div class="card">
+              <video class="card-img-top" controls>
+                <source src="${example.videoUrl}" type="video/mp4">
+              </video>
+              <div class="card-body">
+                <h5 class="card-title">${example.title}</h5>
+                <p class="card-text">${example.description}</p>
+                <p class="card-text mt-2">${example.details}</p>
+              </div>
+            </div>
+          </div>`;
+      exampleRender.true;
+      document.getElementById("examplesCard").style.display = "flex";
+    });
+  } else {
+    exampleCard.innerHTML = "";
+    exampleRender = false;
+  }
+}
+
+// Sayfa Yüklendiginde fonksiyonu tetikle
+document.addEventListener("DOMContentLoaded", (e) => {
+  ExampleShow();
+});
